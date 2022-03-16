8f5c63800be4b97911a07e5666591d3f69e3e073
Mark G <mark.garcia.8001@gmail.com>
2022-03-16 16:19:22 -0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Project2_Virtual_Reality.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project2_Virtual_Reality.dir/main.cpp.obj"
+  "Project2_Virtual_Reality.exe"
+  "Project2_Virtual_Reality.exe.manifest"
+  "Project2_Virtual_Reality.pdb"
+  "libProject2_Virtual_Reality.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project2_Virtual_Reality.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
